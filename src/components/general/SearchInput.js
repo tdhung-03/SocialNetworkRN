9e9c6ccc4fd661e6528966219bbc17b13247b575
Tran Duy Hung <mspotify208@gmail.com>
2023-10-31 23:17:57 +0700

    input

--- src/components/general/SearchInput.js
+++ src/components/general/SearchInput.js
@@ -1,5 +1,17 @@
+import {TextInput, StyleSheet} from 'react-native';
+
 const SearchInput = () => {
-  return;
+  return (
+    <TextInput style={styles.input} placeholder="Search by developer name" />
+  );
 };
 
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: 'white',
+    width: 365,
+    borderRadius: 5,
+    paddingHorizontal: 15,
+  },
+});
 export default SearchInput;

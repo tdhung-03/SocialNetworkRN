ee59aa2832bf95f0d67155273f35295033bd9b7a
Tran Duy Hung <mspotify208@gmail.com>
2023-10-31 19:32:34 +0700

    SVG

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import {View} from 'react-native';
+import Logo from '../assets/images/logo.svg';
+
+const Header = () => {
+  return (
+    <View>
+      <Logo />
+    </View>
+  );
+};
+
+export default Header;

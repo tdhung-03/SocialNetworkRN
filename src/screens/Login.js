5497d789158739e3d2bb401c14c0218670688fa5
Tran Duy Hung <mspotify208@gmail.com>
2023-11-01 09:53:40 +0700

    Login header

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,4 +1,4 @@
-import {View, Text, StyleSheet, ImageBackground} from 'react-native';
+import {View, Text, StyleSheet, ImageBackground, Image} from 'react-native';
 
 const Login = () => {
   return (
@@ -7,7 +7,19 @@ const Login = () => {
         style={styles.background}
         source={require('../assets/images/pattern.jpg')}
         resizeMode="repeat">
-        <View style={styles.overlay}></View>
+        <View style={styles.overlay}>
+          <View style={styles.container}>
+            <View style={styles.header}>
+              <Image
+                source={require('../assets/images/favicon-3.png')}
+                style={styles.logo}></Image>
+              <Text style={styles.textHeader}>Account Login</Text>
+              <Text style={styles.textIntro}>
+                Hello Developer, Welcome Back!
+              </Text>
+            </View>
+          </View>
+        </View>
       </ImageBackground>
     </>
   );
@@ -20,6 +32,32 @@ const styles = StyleSheet.create({
   overlay: {
     flex: 1,
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
+  container: {
+    flex: 0.5,
+    backgroundColor: 'white',
+    borderRadius: 8,
+    paddingVertical: 20,
+  },
+  header: {
+    alignItems: 'center',
+  },
+  logo: {
+    height: 60,
+    width: 60,
+  },
+  textHeader: {
+    fontSize: 26,
+    fontWeight: '800',
+    color: '#2d2d39',
+    marginTop: 9,
+  },
+  textIntro: {
+    fontSize: 15,
+    color: '#737373',
+    marginTop: 4,
   },
 });
 

e41c09a011016c644efbc1bb0a57859039e8318a
Tran Duy Hung <mspotify208@gmail.com>
2023-11-05 11:35:39 +0700

    login btn

--- src/components/general/Header.js
+++ src/components/general/Header.js
@@ -1,6 +1,9 @@
 import {View, StyleSheet, Pressable} from 'react-native';
 import Logo from '../../assets/images/logo.svg';
 import {useNavigation} from '@react-navigation/native';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faArrowAltCircleRight} from '@fortawesome/free-regular-svg-icons/faArrowAltCircleRight';
+import {faArrowAltCircleLeft} from '@fortawesome/free-regular-svg-icons/faArrowAltCircleLeft';
 
 const HorizontalLine = () => {
   return <View style={styles.line}></View>;
@@ -12,11 +15,22 @@ const Header = () => {
     navigation.navigate('Developers');
   };
 
+  const returnLogin = () => {
+    navigation.navigate('Login');
+  };
+
   return (
     <>
       <View style={styles.container}>
         <Pressable onPress={returnDevelopers}>
           <Logo height={30} />
+        </Pressable>
+        <Pressable style={{marginRight: 20}} onPress={returnLogin}>
+          <FontAwesomeIcon
+            icon={faArrowAltCircleRight}
+            size={30}
+            style={styles.icon}
+          />
         </Pressable>
       </View>
       <HorizontalLine />
@@ -29,10 +43,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#2d2d39',
     paddingTop: 17,
     paddingBottom: 17,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   line: {
     height: 1,
     backgroundColor: 'gray',
+  },
+  icon: {
+    color: 'white',
   },
 });
 
